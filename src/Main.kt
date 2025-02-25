a3faff27612b087c971286c1b517010bfa6ae2e9
Raven Rainbow <jdrainbow@waimea.school.nz>
2025-02-25 14:27:52 +1300

    Notes complete
    finished documentation of collection

--- src/Main.kt
+++ src/Main.kt
@@ -25,42 +25,66 @@ fun main() {
 
 
     //show specific items using index
-    println(snacks[0])
+    println("the item at index 0 is ${snacks[0]}")
+
     //println(snacks[4]) this would be out of bound
 
     //change a value
     snacks[1] = "Dog biscuits"
 
-    println(snacks)
+    println("change a value at index 1 $snacks")
 
     //remove a item
     snacks.removeAt(0)
 
-    println(snacks)
+    println("remove at index 0 $snacks")
 
     //add in new items at specific places
     snacks.add(1,"Doritos")
 
-    println(snacks)
+    println("added doritos $snacks")
 
     //sort list
     snacks.sort()
 
-    println(snacks)
+    println("sorted: $snacks")
 
     //shuffle the list (randomise order)
     snacks.shuffle()
 
-    println(snacks)
+    println("Shuffled:$snacks")
 
     //get a random item from list
     val item = snacks.random()
 
-    println(item.toString())
+    println("random item:  ${item.toString()}")
+
+    //searching for item
+    snacks.contains("Chips") //returns boolean
+
+    println("Chips is in list: ${snacks.contains("Chips")}")
+    println("Money is in list: ${snacks.contains("Money")}")
+
+    //getting index of item
+    println("Chips index: ${snacks.indexOf("Chips")}")
+    println("Money index: ${snacks.indexOf("Money")}")
 
     //loop through list
     for (snack in snacks) {
         println(snack.toString())
     }
+
+    //loop using index
+    for (i in 0 until snacks.size) {
+        println("$i ${snacks[i].toString()}")
+    }
+    println()
+    //loop using index and values
+    for ((i, snack) in snacks.withIndex()) {
+        println("$i: ${snack.toString()}")
+    }
+
+
+
 }
 
